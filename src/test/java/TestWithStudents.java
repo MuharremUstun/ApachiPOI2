93770b6d341c2d0211623d0f900b47730d1370b7
MuharremUstun <56238963+MuharremUstun@users.noreply.github.com>
2020-06-09 02:11:25 -0400

    students.xlsx DataProvider example

---
+++ src/test/java/TestWithStudents.java
@@ -0,0 +1,44 @@
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class TestWithStudents {
+    private final static String FILE_NAME = "src/test/resources/students.xlsx";
+
+    @DataProvider(name = "excelData")
+    public Object[][] excelData() throws IOException {
+        FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+        Workbook workbook = new XSSFWorkbook(excelFile);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        int firstRow = sheet.getFirstRowNum();
+        int lastRow = sheet.getLastRowNum();
+        int rowCount = lastRow - firstRow + 1;
+
+        Row row = sheet.getRow(1);
+        int firstCell = row.getFirstCellNum();
+        int lastCell = row.getLastCellNum();
+        int cellCount = lastCell - firstCell;
+
+        Object[][] resultData = new Object[rowCount][cellCount];
+        for (int i = firstRow; i <= lastRow; i++) {
+            row = sheet.getRow(i);
+            for (int j = firstCell; j < lastCell; j++) {
+                resultData[i][j] = row.getCell(j).toString();
+            }
+        }
+        return resultData;
+    }
+
+    @Test(dataProvider = "excelData")
+    public void test3(String c1, String c2, String c3, String c4, String c5, String c6){
+        System.out.print(c1 + ", " + c2 + ", " + c3+ ", " + c4 + ", " + c5 + ", " + c6);
+    }
+}
